702d1ba2f2d4e098dd39fb9c444ca30ae3e7a369
clay4221 <153689264+clay4221@users.noreply.github.com>
2023-12-13 01:56:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+import confetti from "https://cdn.skypack.dev/canvas-confetti";
+
+confetti();
+confetti();
+confetti();
+confetti();
+confetti();
+confetti();
+confetti();
+confetti();
+
+window.addEventListener("click", () => {
+  confetti();
+});
+
+
